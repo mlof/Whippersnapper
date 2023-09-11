0362dc546853838a69dc07f578a615911d55342e
Marcel <marcelrlof@gmail.com>
2023-09-11 21:07:00 +0200

    added migration for transcriptions table

---
+++ src/Whippersnapper/Migrations/20230911190507_Add.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Whippersnapper.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transcriptions",
+                columns: table => new
+                {
+                    Id = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TranscriptionText = table.Column<string>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    ChannelId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    ContainsBadWords = table.Column<bool>(type: "INTEGER", nullable: false),
+                    GuildName = table.Column<string>(type: "TEXT", nullable: false),
+                    ChannelName = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transcriptions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transcriptions");
+        }
+    }
+}
